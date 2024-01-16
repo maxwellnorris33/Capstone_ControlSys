d2708ca493822859f6434120bfd0ebedff8d142a
Blankk011 <khanh.pham1@ucalgary.ca>
2024-01-16 11:20:50 -0700

    made prototype control system meeting desired specs

--- Matlab_Simulink/1. Learning/2. Control System/4. MIMO Control/rsys_model.m
+++
@@ -1,7 +0,0 @@
-function [XDOT] = rsys_model(X, U)
-
-reduced_sys = load('reduced_ss@85ms_straight_and_level.mat').rsys; %input the reduced model here
-XDOT = reduced_sys.A*X + reduced_sys.B*U;
-
-end
-
