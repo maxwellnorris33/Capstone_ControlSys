5222dc75f40eec358094f1bc0e028651c0dd08c6
Blankk011 <khanh.edamame@gmail.com>
2024-03-12 12:07:11 -0600

    implemented correct deflections + pwm min maxes for control surfae deflections

--- 2. Implementation Prototype/4. Log Analysis/1. Mar10_2024/1. Flight #1/ParseLog.m
+++ 2. Implementation Prototype/4. Log Analysis/1. Mar10_2024/1. Flight #1/ParseLog.m
@@ -26,11 +26,11 @@ aileron_left_pwm = sliced_log.RCOU.C5;
 %measurements to map PWM to. Right now the mapping indicates that trim is
 %not 0 deflection.
 
-vtail_right_def = interp1([1780, 1155],[28, -20], mean(vtail_right_pwm)) %1400 is trim
-vtail_left_def = interp1([1805, 1138],[-20, 28], mean(vtail_left_pwm)) %1510 is trim
+vtail_right_def = interp1([800, 2120],[24.5, -24], mean(vtail_right_pwm)) %1400 is trim
+vtail_left_def = interp1([2200, 800],[-26, 23.5], mean(vtail_left_pwm)) %1510 is trim
 
 %convert avg aileron pwms to deflection angles
 
 %no data for mapping yet, need max min pwm and corresponding deflections
-aileron_right_def = interp1([1695, 1398],[28, -20], mean(aileron_right_pwm)) %1600 is trim
-aileron_left_def = interp1([1695, 1398],[28, -20], mean(aileron_left_pwm)) %1400 is trim
+aileron_right_def = interp1([2200, 1004],[16, -10], mean(aileron_right_pwm)) %1600 is trim
+aileron_left_def = interp1([2000, 907],[-11.5, 15.5], mean(aileron_left_pwm)) %1400 is trim
