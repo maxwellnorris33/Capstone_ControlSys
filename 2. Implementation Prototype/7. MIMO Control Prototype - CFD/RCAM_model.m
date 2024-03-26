d5d9e2ec7a7cb6c0fcd1ab5e02b9e496160be8e6
Jared <jaredrc8@gmail.com>
2024-03-26 09:24:13 -0600

    CFD aero coeffs implemented and trimmed states found

---
+++ 2. Implementation Prototype/7. MIMO Control Prototype - CFD/RCAM_model.m
@@ -0,0 +1,168 @@
+%tutorial from christopher lum
+function [XDOT] = RCAM_model(X,U)
+
+%--------------------------STATE AND CONTROL VECTORS-----------------------
+%Extract state vectors
+x1 = X(1); %u
+x2 = X(2); %v
+x3 = X(3); %w
+x4 = X(4); %p
+x5 = X(5); %q
+x6 = X(6); %r
+x7 = X(7); %phi
+x8 = X(8); %theta
+x9 = X(9); %psi
+% x10 = X(10); %PN
+% x11 = X(11); %PE
+% x12 = X(12); %PD
+
+u1 = U(1); %d_A (aileron) +is right roll
+u2 = U(2); %d_T (stabilizer) +is down deflection
+u3 = U(3); %d_R (rudder) +is right deflection
+u4 = U(4); %d_th1 (throttle 1)
+
+%--------------------------CONSTANTS---------------------------------------
+%Nominal Vehicle constants
+m = 5.73; %aircraft total mass (kg) 
+
+cbar = 0.22283;                 %mean aerodynamic chord (m)
+S = 0.51220761;                    %wing planform area (m^2)
+b = 2.176;
+
+Xcg = 0.0631;            %x position of CoG in Fm (m) 
+Ycg = 0;                    %y position of CoG in Fm (m)
+Zcg = -0.04;            %z position of CoG in Fm (m)
+
+Xac = 0.0822;            %x position of AC in Fm (m)
+Yac = 0;                    %y position of AC in Fm (m)
+Zac = 0;                    %z position of AC in Fm (m)
+
+%Engine Constants %NEED THIS
+Xapt1 = 0.75531;                 %est rn %x position of engine 1 force in Fm (m)
+Yapt1 = 0;              %y position of engine 1 force in Fm (m)
+Zapt1 = -0.02;               %z position of engine 1 force in Fm (m)
+
+%Other constants
+rho = 1.225;                %air density (kg/m^3)
+g = 9.81;                   %gravitational acceleration (m/s^2)
+
+%--------------------------2. INTERMEDIATE VARIABLES---------------------
+%calculate airspeed
+Va = sqrt(x1^2 +x2^2 +x3^2);
+
+%calculate alpha and beta
+alpha = atan2(x3, x1);
+beta = asin(x2/Va);
+
+%calculate dynamic pressure
+Q = 0.5*rho*Va^2;
+
+%define vectors wbe_b and V_b
+wbe_b = [x4;x5;x6];
+V_b = [x1;x2;x3];
+
+
+%----------------------3. AERODYNAMIC FORCE COEFFICIENTS-------------------
+%coeffs from openVSP is in windframe, need to rotate to body frame
+
+%total lift force
+CL = 0.762 + 5.28*alpha - 13.5*alpha^2;
+
+%Total Drag Force 
+CD = 0.124 + 0.812*alpha + 4.53*alpha^2;
+
+%Calculating Sideforce
+% OpenVSP: CY = -0.176*(beta) - 0.0423*u3;
+CY = 0.381855937977578*beta - 0.165392960537476*u3;
+%----------------------4. DIMENSIONAL AERODYNICAL FORCES-------------------
+%calculate the actual dimensional forces. These are in Fw
+
+FA_w = [-CD*Q*S; CY*Q*S; -CL*Q*S];
+
+%rotate these forces to Fb
+C_bw = [cos(beta)*cos(alpha) -sin(beta)*cos(alpha) -sin(alpha); sin(beta) cos(beta) 0; cos(beta)*sin(alpha) -sin(beta)*sin(alpha) cos(alpha)];
+FA_b = C_bw*FA_w;
+
+%--------------------6. AERODYNAMIC MOMENT ABOUT CG------------------------
+%normalize to aerodynamic moment about cog
+
+%these moments are in the wind frame, need to rotate to body frame
+% OpenVSP: MAcg_w = [(0.0498849*beta + 0.5630736*x4 + 0.2814754*u1); %roll
+%     (0.0567244-0.1194170*(alpha) - 8.9756*x5 + -1.1049921*u2); %pitch
+%     (-0.0395123*beta + 0.0595315*x6 -0.0207352*u3)]*Q*S*cbar; %yaw
+
+%if pitching up (u2>0), coefficient is different than pitching down
+if u2 >= 0
+    MAcg_w = [(0.5630736*x4 - 0.218178645081431*beta + 2.21808931113144*u1); %roll
+        (-8.9756*x5 -0.0487 - 0.422*alpha + 0.637*alpha^2 + 1.37862002457027*u2); %pitch
+        (0.0595315*x6 + 0.723911207831643*beta - 0.450367297691894*u3)]*Q*S*cbar; %yaw
+else
+    MAcg_w = [(0.5630736*x4 - 0.218178645081431*beta + 2.21808931113144*u1); %roll
+        (-8.9756*x5 -0.0487 - 0.422*alpha + 0.637*alpha^2 + 0.661041663087895*u2); %pitch
+        (0.0595315*x6 + 0.723911207831643*beta - 0.450367297691894*u3)]*Q*S*cbar; %yaw
+end
+
+%rotated moments to body frame
+MAcg_b = C_bw*MAcg_w ;
+
+%-------------------8. ENGINE FORCE AND MOMENT-----------------------------
+% Effect of engine. Calculate thrust force of engine
+if u4 <= 0
+    F1 = 0;
+elseif u4<0.22 & u4>0
+    F1 = 1.2*u4;
+else
+    F1 = 6.457*u4^2-0.9309*u4+0.1751;
+end
+
+F1 = F1*g; %convert thrust test curve from kg to N
+
+%assuming engine thrust is aligned with Fb, we have
+FE1_b = [F1;0;0];
+
+FE_b = FE1_b; 
+
+%now engine moment due to offset of engine thrust from CG
+mew1 = [Xcg - Xapt1; Yapt1 - Ycg; Zcg - Zapt1];
+
+MEcg1_b = cross(mew1, FE1_b);
+
+MEcg_b = MEcg1_b;
+%-------------------9. GRAVITY EFFECTS------------------------------------
+g_b = [-g*sin(x8); g*cos(x8)*sin(x7); g*cos(x8)*cos(x7)];
+
+Fg_b = m*g_b;
+
+%-------------------10. STATE DERIVATIVES---------------------------------
+%inertia matrix
+Ib = m*[0.0802 0 0.108; 
+        0 0.175 0; 
+        0.108 0 0.1084]; %NEED THIS
+
+%inverse of inertia matrix
+invIb = (1/m)*[-36.4944 0 36.3597; 
+    0 5.7143 0; 
+     36.3597 0 -27.0005];
+
+%form f_b and calculate udot, vdot, wdot
+F_b = Fg_b + FE_b + FA_b;
+x1to3dot = (1/m)*F_b - cross(wbe_b, V_b);
+
+%form Mcg_b and calculate pdot, qdot and rdot
+Mcg_b = MAcg_b + MEcg_b;
+x4to6dot = invIb*(Mcg_b - cross(wbe_b, Ib*wbe_b));
+
+%calculate phidot, thetadot and psidot
+H_phi = [1 sin(x7)*tan(x8) cos(x7)*tan(x8); 0 cos(x7) -sin(x7); 0 sin(x7)/cos(x8) cos(x7)/cos(x8)];
+
+x7to9dot = H_phi*wbe_b;
+
+%place in first order form
+
+XDOT = [x1to3dot; x4to6dot; x7to9dot];
+end
+
+
+
+
+
---
+++ 2. Implementation Prototype/7. MIMO Control Prototype - CFD/initializeConstantsMIMO_LQR_tuning.m
@@ -0,0 +1,119 @@
+%keilan changes in case i lose
+
+%this script generates the reduced system statespace and the K gains for
+%the MIMO control system. These variables will be generated into the
+%workspace which will then be automatically saved for use in other scripts
+
+clear 
+close all
+clc
+
+%extracting statespace A, B, C and D matrix
+linearizedSS = load('rcam_linearized_ss20ms_straight_and_level.mat'); %varname is linsys1
+
+A = linearizedSS.linsys1.A;
+B = linearizedSS.linsys1.B;
+C = linearizedSS.linsys1.C;
+D = linearizedSS.linsys1.D;
+
+%synthesizing matlab statespace system
+sys = ss(A, B, C, D);
+
+%truncating system
+rsys = modred(sys, [2;4;6;7;8;9], "Truncate"); 
+% x1 (uvel), x3 (zvel), x5 pitch rate, x10 (altitude) remaining as these will be measurable
+% states
+
+% will use the reduced system to design K gains matrix
+A = rsys.A;
+B = rsys.B;
+C = rsys.C;
+D = rsys.D;
+
+%checking observability and controllability
+Co = ctrb(A, B);
+Ob = obsv(A, C);
+
+if rank(Co) == rank(Ob) & (length(A) - rank(Co)) == 0 & (length(A) - rank(Ob)) == 0;
+    disp('rsys is observable and controllable');
+else
+    disp('rsys is NOT observable or controllable');
+end
+
+%LQR tuning
+
+%the larger the number for Q, the more the controller focuses on that state
+Sys = ss(rsys.A,rsys.B,rsys.C,rsys.D);
+Q_Sys = [0.1 0 0 0; %horizontal vel
+    0 1 0 0; %vertical vel
+    0 0 1 0; %pitch rate
+    0 0 0 1]; %altitude
+
+%the large the number for R, the less control deflection is use
+R_Sys = [1 0 0 0; %aileron
+    0 1. 0 0; %elevator
+    0 0 1 0; %rudder
+    0 0 0 15]; %throttle
+
+[P,~,~] = care(Sys.A,Sys.B,Q_Sys,R_Sys);
+K_LQR = -inv(R_Sys)*Sys.B'*P;
+
+%saving LQR K matrix
+save('k_gains_LQR', "K_LQR")
+
+%getting statespace from the full model linearization
+linear_sys = load("rcam_linearized_ss20ms_straight_and_level.mat").linsys1;
+A = linear_sys.A;
+B = linear_sys.B;
+C = linear_sys.C;
+D = linear_sys.D;
+
+% Define initialized constants
+%initial state @ trim point for straight and level flight @85m/s
+x0 = [20; %inital speed
+    0;
+    -1.0339;
+    0;
+    0;
+    0;
+    0;
+    -0.05172; 
+    0];
+
+%initial control surface deflections
+%input IC trim point for straight and level flight @85m/s
+%since we linearized at the original trim point, all control inputs are
+%centered at zero
+uo = [0;
+    0.0243934;
+    0;
+    0.47803];
+
+TF = 1*60; %how long the sim runs for
+
+%k gain
+k_gain_LQR = load("k_gains_LQR.mat")
+k = k_gain_LQR.K_LQR
+
+%initial longitude and latitude @honolulu airport
+lat0 = convert_coordinates(21, 18, 56.1708);
+lon0 = convert_coordinates(157, 51, 29.1348);
+
+%initial plane altitude (m)
+h0 = 50;
+
+% Define Actuator Saturation Limits
+u1min = -25*pi/180;
+u1max = 25*pi/180;
+
+u2min = -25*pi/180;
+u2max = 10*pi/180;
+
+u3min = -30*pi/180;
+u3max = 30*pi/180;
+
+u4min = 0;
+u4max = 1;
+
+
+
